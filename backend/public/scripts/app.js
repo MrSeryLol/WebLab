31642c7128c73a842cd127614c86d93bf978d38d
MrSeryLol <psygentelmen014@gmail.com>
2023-05-11 15:57:39 +0300

    Lab7 is ready

--- backend/public/scripts/app.js
+++ backend/public/scripts/app.js
@@ -61,44 +61,40 @@ class Channel {
 function inputField() {
     $('.main__bottom-section').append('\
     <span> \
-        <input type="text" placeholder="Введите данные, чтобы добавить новый канал" class="input-field-add"> \
+        <input type="text" placeholder="Что хотите посмотреть?" class="input-field-add"> \
     </span> \
-        <button class="input-field-button">Найти</button> \
+        <button class="input-field-button">Добавить</button> \
     <style>\
         input.text { \
-            width: 200px; \
+            width: 300px; \
         }\
     </style>')
 }
 
-function addNewChannel(tomorrowSchedule, yesterdaySchedule) {
-    const firstChannel = new Channel('first-channel.html', 'first-channel-icon.png', 'Спорт');
-    const firstChannelSchedule = new Schedule(firstChannel, ['04:00'], ['Хоккей'])
-    firstChannelSchedule.setSchedule(firstChannelSchedule.getHTML())
-    console.log(tomorrowSchedule)
-    // $('.yesterday-item').on('click', () => {
-    //     //$('.grid-container').empty()
-    //     const firstChannelSchedule = new Schedule(firstChannel, ['04:00'], ['Хоккей'])
-    //     //const russiaOneChannelSchedule = new Schedule(russiaOneChannel, yesterdayScheduleTime[1], yesterdayScheduleItems[1])
-    //     //const stsChannelSchedule = new Schedule(stsChannel, yesterdayScheduleTime[2], yesterdayScheduleItems[2])
-
-    //     firstChannelSchedule.setSchedule(firstChannelSchedule.getHTML())
-    //     //russiaOneChannelSchedule.setSchedule(russiaOneChannelSchedule.getHTML())
-    //     //stsChannelSchedule.setSchedule(stsChannelSchedule.getHTML())
-    // })
+function addNewChannel(scheduleObjects) {
+    console.log(scheduleObjects)
+    const newChannel = {"dayOfWeek": "Вчера", "channel": "Россия 1", "scheduleTime": ["05:00", "11:00", "14:55", "23:25"], "scheduleProgram": ["Утро России", "Вести", "Кто против?", "Вечер с Владимиром Соловьёвым"]}
+    $.post('addchannel', newChannel, function(res) {
+        console.log("Мы отправили данные и получили ответ сервера!");
+        console.log(res);
+        scheduleObjects.push(newChannel)
+    })
 }
-
 
 $(document).ready(() => {
     console.log("hello, world!");
 
-    $.getJSON("./static/jsonFiles/yesterdayTvPrograms.json", scheduleObjects => {
+    $.getJSON("/yesterdayTvPrograms.json", scheduleObjects => {
         let tomorrowSchedule = new Array();
         let yesterdaySchedule = new Array();
+        let channelInfo = new Array();
+
+        console.log(scheduleObjects)
 
         scheduleObjects.forEach(element => {
             if (element.dayOfWeek === 'Вчера') {
                 yesterdaySchedule.push(element)
+                
             }
             else if (element.dayOfWeek === 'Завтра') {
                 tomorrowSchedule.push(element)
@@ -112,24 +108,50 @@ $(document).ready(() => {
         //const tomorrowChannels = tomorrowSchedule.map(schedule => {return schedule.channel})
         const tomorrowScheduleTime = tomorrowSchedule.map(schedule => {return schedule.scheduleTime})
         const tomorrowScheduleItems = tomorrowSchedule.map(schedule => {return schedule.scheduleProgram})
+
+        for(let i = 0; i < yesterdayChannels.length; i++) {
+            if (yesterdayChannels[i] === 'Первый канал') {
+                const firstChannel = new Channel('first-channel.html', 'first-channel-icon.png', yesterdayChannels[i]);
+                const firstChannelSchedule = new Schedule(firstChannel, yesterdayScheduleTime[i], yesterdayScheduleItems[i])
+                channelInfo.push(firstChannelSchedule)
+            }
+            else if (yesterdayChannels[i] === 'Россия 1') {
+                const russiaOneChannel = new Channel('russia-1-channel.html', 'Russia-one-icon.png', yesterdayChannels[i]);
+                const russiaOneChannelSchedule = new Schedule(russiaOneChannel, yesterdayScheduleTime[i], yesterdayScheduleItems[i])
+                channelInfo.push(russiaOneChannelSchedule)
+            }
+            else if (yesterdayChannels[i] === 'СТС'){
+                const stsChannel = new Channel('sts-channel.html', 'Logo_СТС_23-10-2017.png', yesterdayChannels[i]);
+                const stsChannelSchedule = new Schedule(stsChannel, yesterdayScheduleTime[i], yesterdayScheduleItems[i])
+                channelInfo.push(stsChannelSchedule)
+            }
+        }
+
+        console.log(channelInfo)
         
-        const firstChannel = new Channel('first-channel.html', 'first-channel-icon.png', yesterdayChannels[0]);
-        const russiaOneChannel = new Channel('russia-1-channel.html', 'Russia-one-icon.png', yesterdayChannels[1]);
-        const stsChannel = new Channel('sts-channel.html', 'Logo_СТС_23-10-2017.png', yesterdayChannels[2]);
+        // const firstChannel = new Channel('first-channel.html', 'first-channel-icon.png', yesterdayChannels[0]);
+        // const russiaOneChannel = new Channel('russia-1-channel.html', 'Russia-one-icon.png', yesterdayChannels[1]);
+        // const stsChannel = new Channel('sts-channel.html', 'Logo_СТС_23-10-2017.png', yesterdayChannels[2]);
 
         $('.yesterday-item').on('click', () => {
             $('.grid-container').empty()
-            const firstChannelSchedule = new Schedule(firstChannel, yesterdayScheduleTime[0], yesterdayScheduleItems[0])
-            const russiaOneChannelSchedule = new Schedule(russiaOneChannel, yesterdayScheduleTime[1], yesterdayScheduleItems[1])
-            const stsChannelSchedule = new Schedule(stsChannel, yesterdayScheduleTime[2], yesterdayScheduleItems[2])
 
-            firstChannelSchedule.setSchedule(firstChannelSchedule.getHTML())
-            russiaOneChannelSchedule.setSchedule(russiaOneChannelSchedule.getHTML())
-            stsChannelSchedule.setSchedule(stsChannelSchedule.getHTML())
+            // const firstChannelSchedule = new Schedule(firstChannel, yesterdayScheduleTime[0], yesterdayScheduleItems[0])
+            // const russiaOneChannelSchedule = new Schedule(russiaOneChannel, yesterdayScheduleTime[1], yesterdayScheduleItems[1])
+            // const stsChannelSchedule = new Schedule(stsChannel, yesterdayScheduleTime[2], yesterdayScheduleItems[2])
 
-            $('.input-field-button').on('click', function() {
-                addNewChannel(tomorrowSchedule, yesterdaySchedule)
+            channelInfo.forEach(element => {
+                element.setSchedule(element.getHTML())
+                console.log(element)
             })
+
+            // firstChannelSchedule.setSchedule(firstChannelSchedule.getHTML())
+            // russiaOneChannelSchedule.setSchedule(russiaOneChannelSchedule.getHTML())
+            // stsChannelSchedule.setSchedule(stsChannelSchedule.getHTML())
+
+            // $('.input-field-button').on('click', function() {
+            //     addNewChannel(scheduleObjects)
+            // })
         })
 
         $('.tomorrow-item').on('click', () => {
@@ -141,6 +163,15 @@ $(document).ready(() => {
             firstChannelSchedule.setSchedule(firstChannelSchedule.getHTML())
             russiaOneChannelSchedule.setSchedule(russiaOneChannelSchedule.getHTML())
             stsChannelSchedule.setSchedule(stsChannelSchedule.getHTML())
+        })
+
+        $('.input-field-button').on('click', function() {
+            addNewChannel(scheduleObjects)
+        })
+
+        channelInfo.forEach(element => {
+            element.setSchedule(element.getHTML())
+            console.log(element)
         })
     })
 
